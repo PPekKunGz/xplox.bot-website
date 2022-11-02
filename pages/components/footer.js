8d6e604144985d3287084be2a55267c0c1c8c8da
PPekKunGz <54957742+PPekKunGz@users.noreply.github.com>
2022-11-02 11:24:35 +0700

    Update footer.js

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -38,8 +38,6 @@ function Footer() {
               href="http://www.facebook.com/share.php?u=https://xplox.vikoperation.studio/"
               onclick="window.open(encodeURI(decodeURI(this.href)), 'FBwindow', 'width=650, height=470, menubar=no, toolbar=no, scrollbars=yes'); return false;"
               rel="nofollow"
-              rel="noreferrer"
-              target="_blank"
             >
               <FacebookIcon className={footer.icon} />
             </a>
